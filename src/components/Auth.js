931552e8c43f126959b2a5ee6988868f48b6541a
CandeMarcote <candemarcote@gmail.com>
2022-05-06 17:51:06 -0300

    redux with multiple slices

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,40 @@
+import React, { useRef } from 'react';
 import classes from './Auth.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 
 const Auth = () => {
+  const isAuth = useSelector(state => state.auth.auth);
+  const dispatch = useDispatch();
+
+  const emailInputRef = useRef('');
+  const passwordInputRef = useRef('');
+
+  function logInHandler(e) {
+    e.preventDefault();
+
+    if(emailInputRef.current.value.trim() === '' || !emailInputRef.current.value.includes('@') || passwordInputRef.current.value.trim() === '') {
+      return;
+    }
+    console.log(emailInputRef.current.value)
+    console.log(passwordInputRef.current.value)
+    dispatch(authActions.isLoggedIn())
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={logInHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <input type='email' id='email' ref={emailInputRef} />
           </div>
+
           <div className={classes.control}>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input type='password' id='password' ref={passwordInputRef} />
           </div>
-          <button>Login</button>
+          {!isAuth && <button type='submit'>Login</button>}
         </form>
       </section>
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,16 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 
 const Header = () => {
+  const isAuth = useSelector(state => state.auth.auth);
+  const dispatch = useDispatch();
+
+  function logoutHandler() {
+    dispatch(authActions.isLoggedOut)
+  }
+
+  
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -13,7 +23,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {isAuth && <button onClick={logoutHandler}>Logout</button>}
           </li>
         </ul>
       </nav>
